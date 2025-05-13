873e99009272c647ae552c456c7f6a0a9a9086f5
Ranjan Sitikhu <ranjansitikhu@gmail.com>
2025-05-13 18:10:06 +0100

    Initial Commit

---
+++ PokemonDemo/PokemonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDemoApp.swift
+//  PokemonDemo
+//
+//  Created by MacBook on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
